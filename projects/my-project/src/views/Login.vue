0a34138fd76a941947f4406c39ed7c252a4efaec
苳苳 <1528507854@qq.com>
2020-06-23 08:29:39 +0800

    登录页面完成

---
+++ projects/my-project/src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="login">
+    <div class="login-box">
+      登录模块
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style lang="less" scoped>
+#login {
+  width: 100%;
+   height: 100%;
+  background-color: #2b4b6b;
+}
+.login-box {
+  width: 450px;
+  height: 300px;
+  background-color: #ffffff;
+  position: sticky;
+    top:50%;
+    left:50%;
+ transform: translate(-50%, -50%);
+}
+</style>
